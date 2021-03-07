19ea160815d3eec914c8c24051db9663cd35891d
ilja615 <615ilja@gmail.com>
2021-03-07 09:38:31 +0100

    Add koi and blobfish compat

--- src/main/java/com/github/ilja615/fish_in_planks/ModBlocks.java
+++ src/main/java/com/github/ilja615/fish_in_planks/ModBlocks.java
@@ -10,20 +10,15 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModBlocks
 {
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModMain.MOD_ID);
-    public static final RegistryObject<Block> COD_BARREL = BLOCKS.register("cod_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_COD_FLOP));
-    public static final RegistryObject<Block> SALMON_BARREL = BLOCKS.register("salmon_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_SALMON_FLOP));
-    public static final RegistryObject<Block> TROPICAL_FISH_BARREL = BLOCKS.register("tropical_fish_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_TROPICAL_FISH_FLOP));
-    public static final RegistryObject<Block> PUFFERFISH_BARREL = BLOCKS.register("pufferfish_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_PUFFER_FISH_FLOP));
+    public static RegistryObject<Block> COD_BARREL;
+    public static RegistryObject<Block> SALMON_BARREL;
+    public static RegistryObject<Block> TROPICAL_FISH_BARREL;
+    public static RegistryObject<Block> PUFFERFISH_BARREL;
 
     public static RegistryObject<Block> PIKE_BARREL;
     public static RegistryObject<Block> LIONFISH_BARREL;
-    public static void registerUACompatBlocks()
-    {
-        if (ModList.get().isLoaded("upgrade_aquatic"))
-        {
-            PIKE_BARREL = BLOCKS.register("pike_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_SALMON_FLOP));
-            LIONFISH_BARREL = BLOCKS.register("lionfish_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_PUFFER_FISH_FLOP));
-        }
-    }
+
+    public static RegistryObject<Block> KOI_BARREL;
+
+    public static RegistryObject<Block> BLOBFISH_BARREL;
 }
--- src/main/java/com/github/ilja615/fish_in_planks/ModMain.java
+++ src/main/java/com/github/ilja615/fish_in_planks/ModMain.java
@@ -1,10 +1,13 @@
 package com.github.ilja615.fish_in_planks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FireBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,7 +16,13 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static com.github.ilja615.fish_in_planks.ModMain.MOD_ID;
 
@@ -22,16 +31,37 @@ public class ModMain
 {
     public static final String MOD_ID = "fish_in_planks";
     public static final Item.Properties ITEM_PROPERTY = new Item.Properties().group(ItemGroup.BUILDING_BLOCKS);
+    public static ModMain INSTANCE;
 
     public ModMain()
     {
-        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        INSTANCE = this;
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
         modEventBus.addListener(this::setupCommon);
 
-        //ModItems.ITEMS.register(modEventBus);
-        ModBlocks.registerUACompatBlocks();
-        ModBlocks.BLOCKS.register(modEventBus);
+        DeferredRegister<Block> blocks = makeRegister(modEventBus, ForgeRegistries.BLOCKS);
+        DeferredRegister<Item> items = makeRegister(modEventBus, ForgeRegistries.ITEMS);
 
+        ModBlocks.COD_BARREL = registerBlockAndItem(blocks, items, "cod_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_COD_FLOP), block -> new BlockItem(block, ITEM_PROPERTY));
+        ModBlocks.SALMON_BARREL = registerBlockAndItem(blocks, items, "salmon_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_SALMON_FLOP), block -> new BlockItem(block, ITEM_PROPERTY));
+        ModBlocks.TROPICAL_FISH_BARREL = registerBlockAndItem(blocks, items, "tropical_fish_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_TROPICAL_FISH_FLOP), block -> new BlockItem(block, ITEM_PROPERTY));
+        ModBlocks.PUFFERFISH_BARREL = registerBlockAndItem(blocks, items, "pufferfish_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_PUFFER_FISH_FLOP), block -> new BlockItem(block, ITEM_PROPERTY));
+
+        if (ModList.get().isLoaded("upgrade_aquatic"))
+        {
+            ModBlocks.PIKE_BARREL = registerBlockAndItem(blocks, items, "pike_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_SALMON_FLOP), block -> new BlockItem(block, ITEM_PROPERTY));
+            ModBlocks.LIONFISH_BARREL = registerBlockAndItem(blocks, items, "lionfish_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_PUFFER_FISH_FLOP), block -> new BlockItem(block, ITEM_PROPERTY));
+        }
+        if (ModList.get().isLoaded("environmental"))
+        {
+            ModBlocks.KOI_BARREL = registerBlockAndItem(blocks, items, "koi_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_COD_FLOP), block -> new BlockItem(block, ITEM_PROPERTY));
+        }
+        if (ModList.get().isLoaded("alexsmobs"))
+        {
+            ModBlocks.BLOBFISH_BARREL = registerBlockAndItem(blocks, items, "blobfish_barrel", () -> new FishBarrelBlock(Block.Properties.from(Blocks.BARREL), SoundEvents.ENTITY_COD_FLOP), block -> new BlockItem(block, ITEM_PROPERTY));
+        }
     }
 
     private void setupCommon(final FMLCommonSetupEvent event)
@@ -47,19 +77,28 @@ public class ModMain
                 fireblock.setFireInfo(ModBlocks.PIKE_BARREL.get(), 5, 5);
                 fireblock.setFireInfo(ModBlocks.LIONFISH_BARREL.get(), 5, 5);
             }
+            if (ModList.get().isLoaded("environmental"))
+            {
+                fireblock.setFireInfo(ModBlocks.KOI_BARREL.get(), 5, 5);
+            }
+            if (ModList.get().isLoaded("alexsmobs"))
+            {
+                fireblock.setFireInfo(ModBlocks.BLOBFISH_BARREL.get(), 5, 5);
+            }
         }
     }
 
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
-            final IForgeRegistry<Item> registry = event.getRegistry();
-            ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(block -> {
-                final BlockItem blockItem = new BlockItem(block, ITEM_PROPERTY);
-                blockItem.setRegistryName(block.getRegistryName());
-                registry.register(blockItem);
-            });
-        }
+    static <T extends IForgeRegistryEntry<T>> DeferredRegister<T> makeRegister(IEventBus modBus, IForgeRegistry<T> registry)
+    {
+        DeferredRegister<T> register = DeferredRegister.create(registry, MOD_ID);
+        register.register(modBus);
+        return register;
+    }
+
+    static <BLOCK extends Block, ITEM extends BlockItem> RegistryObject<BLOCK> registerBlockAndItem(DeferredRegister<Block> blocks, DeferredRegister<Item> items, String name, Supplier<BLOCK> blockSupplier, Function<BLOCK,ITEM> itemFactory)
+    {
+        RegistryObject<BLOCK> blockObject = blocks.register(name, blockSupplier);
+        items.register(name, () -> itemFactory.apply(blockObject.get()));
+        return blockObject;
     }
 }
